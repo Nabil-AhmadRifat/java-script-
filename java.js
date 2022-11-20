843f545bd166c25a7d773dfe8d5da5f83db5db9d
Nabil-AhmadRIfat <rifat15-5350@diu.edu.bd>
2022-11-20 19:52:41 +0600

    first commit

---
+++ java.js
@@ -0,0 +1,22 @@
+var fs=require('fs');
+var http=require('http');
+
+
+
+http.createServer(function (req,res) {
+    if(req.url="/"){
+        //asy
+        fs.readFile('home.html',function (error,data) {
+        res.writeHead(200,{'content-type':'text/html'});
+        res.write(data);
+        res.end();
+            
+        })
+        
+    }
+
+
+});
+Server.listen(5050);
+//Server.listen(4040);
+console.log("server run success");
